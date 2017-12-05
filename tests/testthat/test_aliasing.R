e49c2f8316899c80ffb139010528ebc46e5a678b
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-12-05 13:38:32 +0000

    Updated tests with babies data

--- tests/testthat/test_aliasing.R
+++ tests/testthat/test_aliasing.R
@@ -1,16 +1,11 @@
 library("plyr")
 
-data("babies", package = "cond")
-
-## clogit understands only 0-1 so expand
-babies_expand <- ddply(babies, ~ lull + day, function(z) {
-    data.frame(y = rep(c(0, 1), c(z$r2, z$r1)))
-})
+data("babies", package = "corzed")
 
 babies_ml0 <- glm(formula = y ~ day + lull - 1,
-                  family = binomial, data = babies_expand)
+                  family = binomial, data = babies)
 babies_ml1 <- glm(formula = y ~ day + lull + I(as.integer(lull) + 2) + I(as.integer(lull) + 3) - 1,
-                  family = binomial, data = babies_expand)
+                  family = binomial, data = babies)
 
 c0 <- corzed(babies_ml0)
 c1 <- corzed(babies_ml1)
--- tests/testthat/test_corzed_ci.R
+++ tests/testthat/test_corzed_ci.R
@@ -1,14 +1,9 @@
 library("plyr")
 
-data("babies", package = "cond")
-
-## clogit understands only 0-1 so expand
-babies_expand <- ddply(babies, ~ lull + day, function(z) {
-    data.frame(y = rep(c(0, 1), c(z$r2, z$r1)))
-})
+data("babies", package = "corzed")
 
 babies_ml0 <- glm(formula = y ~ day + lull - 1,
-                  family = binomial, data = babies_expand)
+                  family = binomial, data = babies)
 
 
 ci95_default <- confint.default(babies_ml0, level = 0.95)
@@ -25,10 +20,10 @@ test_that("corzed_ci returns correct CIs when adjust = FALSE", {
 
 
 babies_ml1 <- glm(formula = y ~ day + lull + I(as.integer(lull) + 2) + I(as.integer(lull) + 3) - 1,
-                  family = binomial, data = babies_expand)
+                  family = binomial, data = babies)
 
-ci95_corzed1 <- corzed_confint(babies_ml1, level = 0.95, adjust = TRUE, which = c(1, 4, 5, 6, 11, 16, 18, 20))
-ci95_corzed2 <- corzed_confint(babies_ml1, level = 0.95, adjust = TRUE, which = names(coef(babies_ml1))[c(1, 4, 5, 6, 11, 16, 18, 20)])
+ci95_corzed1 <- corzed_confint(babies_ml1, level = 0.95, adjust = TRUE, which = c(16, 18, 20))
+ci95_corzed2 <- corzed_confint(babies_ml1, level = 0.95, adjust = TRUE, which = names(coef(babies_ml1))[c(16, 18, 20)])
 
 test_that("which works with both names and indexes in corzed_confint", {
     expect_equal(ci95_corzed1, ci95_corzed1, tolerance = tolerance)
