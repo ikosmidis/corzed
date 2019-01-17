b6ce114a4f3694d688cb67cce4bac8320f98c522
Your NameClo <dicaterina@stat.unipd.it>
2019-01-17 16:14:51 +0100

    new code for cloting simulations

--- inst/supplementary_1710-11217/code/clotting_simulation.R
+++ inst/supplementary_1710-11217/code/clotting_simulation.R
@@ -9,11 +9,67 @@ library("plyr")
 library("dplyr")
 library("doMC")
 
-
 ## Specify number of cores, simulation size and set seed
-registerDoMC(3)
-nsimu <- 50000
+registerDoMC(20)
+nsimu <- 1000
+nboot <- 1000
+eps <- 1e-04
 set.seed(123)
+
+## Bootstrap function
+clotting_bootstrap <- function(full_fit, R = 100, ncores = 1) {
+  simulate_clot <- get_simulate_function(full_fit)
+  obs_data <- full_fit$data
+  full_coef <- coef(full_fit)
+  ncoef <- length(full_coef)
+  
+  zstats_ml <- function(data, psi) {
+    temp <- obs_data
+    temp$conc <- unlist(data)
+    temp_fit <- try(update(full_fit, data = temp))
+    if (inherits(temp_fit, "try-error")) {
+      zstat_ml <- zstat_ml_cor <- rep(NA, ncoef)
+      c(zstat_ml, zstat_ml_cor)
+    }
+    else {
+      temp_coefs <- coef(temp_fit)
+      zstat_ml <- waldi(temp_fit, null = psi, adjust = FALSE, numerical = FALSE)
+      zstat_ml_cor <- waldi(temp_fit, null = psi, adjust  = TRUE, numerical = FALSE)
+      c(zstat_ml, zstat_ml_cor)
+    }
+  }
+  generate_clot <- function(data, mle) {
+    simulate_clot(mle)
+  }
+  
+  zstats_mom <- function(data, psi) {
+    temp <- obs_data
+    temp$conc <- unlist(data)
+    temp_fit <- try(update(full_fit, data = temp))
+    if (inherits(temp_fit, "try-error")) {
+      zstat_mom <- rep(NA, ncoef)
+    }
+    else {
+      temp_coefs <- coef(temp_fit)
+      (temp_coefs - psi)/sqrt(diag(vcov(temp_fit)))
+    }
+  }
+  generate_clot_mom <- function(data, mle) {
+    simulate_clot(mle, dispersion = summary(full_fit)$dispersion)
+  }
+  
+  boot_ml <- boot(obs_data$conc, statistic = zstats_ml,
+                  R = R, sim = "parametric", ran.gen = generate_clot,
+                  mle = full_coef, psi = full_coef,
+                  parallel = "multicore", ncpus = ncores)
+  boot_mom <- boot(obs_data$conc, statistic = zstats_mom, R = R,
+                   sim = "parametric", ran.gen = generate_clot_mom,
+                   mle = full_coef, psi = full_coef,
+                   parallel = "multicore", ncpus = ncores)
+  
+  data.frame(zstat_ml = boot_ml$t[, 1:ncoef], zstat_ml_cor = 
+               boot_ml$t[, ((ncoef + 1):(2*ncoef))], zstat_mom = boot_mom$t)
+}
 
 ## The clotting data set
 clotting <- data.frame(
@@ -23,6 +79,7 @@ clotting <- data.frame(
 
 ## The maximum likelihood fit of the gamma regression model
 model_fit <- glm(conc ~ log(u)*lot, data = clotting, family = Gamma(link = "log"))
+summary(model_fit)
 
 ## Simulate at the maximum likelihood estimates of beta and phi
 clotting_simulate <- get_simulate_function(model_fit)
@@ -34,24 +91,70 @@ n_par <- length(coef(model_fit))
 
 simu_data <- data.frame(t(simu_data))
 simu_data$sample <- seq.int(nsimu)
+## Bootstrap seeds
+simu_data$seeds <- sample(seq.int(nsimu * 100), nsimu, replace = FALSE)
 
-res <- ddply(simu_data, ~ sample, function(response) {
+start <- seq(0, 9, 1) * 100 + 1
+end <- seq(1, 10, 1) * 100
+
+res <- list()
+
+## Perform simulation
+for (j in seq_along(start)) {
+  cdat <- simu_data[start[j]:end[j], ]
+  temp_res <- dlply(cdat, ~ sample, function(response) {
     temp_data <- clotting
-    temp_data$conc <- unlist(response[-(n_obs + 1)])
+    temp_data$conc <- unlist(response[-(n_obs + 1:2)])
     temp_ml_fit <- update(model_fit, data = temp_data)
     temp_br_fit <- update(model_fit, data = temp_data, method = "brglmFit")
-    zstat_ml <- waldi(temp_ml_fit, null = coef(model_fit), adjust = FALSE, numerical = FALSE)
-    zstat_ml_cor <- waldi(temp_ml_fit, null = coef(model_fit), adjust  = TRUE, numerical = FALSE)
-    zstat_rb <- waldi(temp_br_fit, null = coef(model_fit), adjust  = FALSE, numerical = FALSE)
-    zstat_rb_cor <- waldi(temp_br_fit, null = coef(model_fit), adjust  = TRUE, numerical = FALSE)
+    zstat_ml <- waldi(temp_ml_fit, null = coef(model_fit), adjust = FALSE, 
+                      numerical = FALSE)
+    zstat_rb <- waldi(temp_br_fit, null = coef(model_fit), adjust = FALSE, 
+                      numerical = FALSE)
+    zstat_ml_cor <- waldi(temp_ml_fit, null = coef(model_fit), adjust = TRUE, 
+                          numerical = FALSE)
+    zstat_rb_cor <- waldi(temp_br_fit, null = coef(model_fit), adjust = TRUE, 
+                          numerical = FALSE)
     zstat_mom <- (coef(temp_ml_fit) - coef(model_fit))/sqrt(diag(vcov(temp_ml_fit)))
+    zstats_temp <- c(zstat_ml, zstat_ml_cor, zstat_mom)
+    ### bootstrap
+    set.seed(temp_data$seed)
+    boots_conv <- clotting_bootstrap(temp_ml_fit, R = nboot, ncores = 1)
+    conv_mat <- rbind(data.matrix(boots_conv), zstats_temp)
+    p_2sided_conv <- apply(conv_mat, 2, function(col) (sum(abs(col[-(nboot + 1)]) >= 
+                                                             abs(col[nboot + 1])) + eps)/(nboot + 2 * eps))
+    p_left_conv <- apply(conv_mat, 2, function(col) (sum(col[-(nboot + 1)] <= 
+                                                           col[nboot + 1]) + eps)/(nboot + 2 * eps))
+    p_right_conv <- apply(conv_mat, 2, function(col) (sum(col[-(nboot + 1)] >= 
+                                                            col[nboot + 1]) + eps)/(nboot + 2 * eps))
+    
+    ### location- and scale-adjusted Wald statistics
+    ## variance estimated by conventional bootstrap
+    ses_ml_conv <- apply(boots_conv[, 1:n_par], 2, sd)
+    ses_ml_cor_conv <- apply(boots_conv[, (n_par + 1):(2*n_par)], 2, sd)
+    
+    zstat_loc_sc_ml_conv <- zstat_ml_cor/ses_ml_conv   ## p x 1 vector
+    zstat_loc_sc_ml_cor_conv <- zstat_ml_cor/ses_ml_cor_conv
+    
     if (response$sample %% 100 == 0)
-        cat(response$sample, "\n")
-    data.frame(statistic = rep(c("ml", "ml_cor", "rb", "rb_cor", "mom"), each = n_par),
-               value = c(zstat_ml, zstat_ml_cor, zstat_rb, zstat_rb_cor, zstat_mom),
-               parameter = rep(1:4, times = 5))
-}, .parallel = TRUE)
+      cat(response$sample, "\n")
+    stats <- data.frame(name = rep(c("ml", "ml_cor", "rb", "rb_cor", "mom", 
+                                     "sc_ml_conv", "sc_ml_cor_conv"), each = n_par),
+                        value = c(zstat_ml, zstat_ml_cor, zstat_rb, zstat_rb_cor, zstat_mom, 
+                                  zstat_loc_sc_ml_conv, zstat_loc_sc_ml_cor_conv),
+                        parameter = rep(1:n_par, times = 7))
+    boot_pvalues <- data.frame(statistic = c(rep(rep(c("ml", "ml_cor", "mom"), 
+                                                     each = n_par), times = 3)),
+                               value = c(p_2sided_conv, p_right_conv, p_left_conv),
+                               type = rep(c("boot_conv_2sided", "boot_conv_right", 
+                                            "boot_conv_left"), each = 3*n_par),
+                               parameter = rep(rep(1:n_par, times = 3), times = 3))
+    list(stats = stats, boot_pvalues = boot_pvalues)                                           
+  }, .parallel = TRUE)
+  res <- c(res, temp_res)
+  save.image(paste0("~/clotting_simu", end[j], ".rda"))
+}
+
 
 save(nsimu, clotting, model_fit, clotting_simulate, simu_data, n_obs, n_par, simu_data, res,
      file = paste(path, "results/clotting_simulation.rda", sep = "/"))
-
